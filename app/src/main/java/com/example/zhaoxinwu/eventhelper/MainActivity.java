56655655f8e5de9fa6bcf771ecc366845847a4c3
YagiGo <jeremywu1995@gmail.com>
2018-05-22 13:01:06 +0900

    Basic UI

--- app/src/main/java/com/example/zhaoxinwu/eventhelper/MainActivity.java
+++ app/src/main/java/com/example/zhaoxinwu/eventhelper/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_shopping_list) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_shopping_history) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_location_info) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_eshi_info) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_setting) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
